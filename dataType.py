419acb5a9d1440f06c1c20a78cb0919d02e974e9
Muhammad Hanif <hm6289894@gmail.com>
2025-09-19 00:24:30 +0500

    data types

---
+++ dataType.py
@@ -0,0 +1,21 @@
+x = 3 + 5j
+y = 1 + 2j
+
+print(x.real)
+print(x.imag)
+print(x.conjugate)
+print(abs(x))
+print(x+y)
+
+# list
+
+x = ["apple", "banana", "cherry"]
+print(x)
+print(type(x))
+x.append("Guavava")
+x.insert(0, "mango")
+x.remove("Guavava")
+x.pop()
+x.sort()
+x.reverse()
+print(x)
